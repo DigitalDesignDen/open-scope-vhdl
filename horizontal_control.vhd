935e0f5e88d46c37b75927c6e188fc6b65f010ec
DigitalDesignDen <maister2677960@gmail.com>
2025-01-15 11:25:09 +0100

    Implemented horizontal control
    Horizontal control in own module. Controlled by push buttons (KEY[1:0]).
    Increased fixed buffer size to 2240.

--- horizontal_control.vhd
+++ horizontal_control.vhd
@@ -19,20 +19,26 @@ end horizontal_control;
 
 architecture behavioral of horizontal_control is
 
-signal r_numerator : positive range 1 to 4 := 1; 
+signal r_numerator	: natural range 0 to 7 := 0; 
 
-signal r_inc_pulse : std_logic;
+signal r_inc_pulse	: std_logic;
+signal r_dec_pulse	: std_logic;
 
 begin
 
 	U1 : edgedetect
-		port map (i_clock, i_inc, r_inc_pulse, pos_edge);
+		port map (i_clock, i_inc, r_inc_pulse, neg_edge);
+		
+	U2 : edgedetect
+		port map (i_clock, i_dec, r_dec_pulse, neg_edge);
 
 numerator_set : process (i_clock)
 begin
 	if (rising_edge(i_clock)) then
-		if r_inc_pulse = '1' then
+		if r_inc_pulse = '1' AND r_numerator < 7 then
 			r_numerator <= r_numerator + 1;
+		elsif r_dec_pulse = '1' AND r_numerator > 1 then
+			r_numerator <= r_numerator - 1;
 		end if;
 	end if;
 end process numerator_set;
@@ -41,7 +47,7 @@ address_mapping : process (i_videoEN, i_hc, r_numerator)
 begin
 if (i_videoEN = '1') then
 	if ( to_integer(unsigned(i_hc) - 144) * r_numerator )/2 < BUFFER_SIZE then
-		o_addr_read	<= ( to_integer(unsigned(i_hc) - 144) * r_numerator )/2 mod BUFFER_SIZE;
+		o_addr_read	<= ( (to_integer(unsigned(i_hc) - 144) * r_numerator )/2) mod BUFFER_SIZE;
 	else
 		o_addr_read	<= BUFFER_SIZE - 1;
 	end if;
